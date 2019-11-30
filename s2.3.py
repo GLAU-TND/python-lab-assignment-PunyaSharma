81686bf015d6bd880b2da6c67214624f37137a6b
PunyaSharma <46519268+PunyaSharma@users.noreply.github.com>
2019-11-30 16:34:10 +0530

    Add files via upload

---
+++ s2.3.py
@@ -0,0 +1,11 @@
+c=(2,1,3,4,5,6)
+try:
+    c.remove(5)
+except AttributeError as e:
+    print("AttributeError",e)
+except TypeError as e:
+    print("TypeError",e)
+except ValueError as e:
+    print("ValueError",e)
+finally:
+    print("Process Complete")
