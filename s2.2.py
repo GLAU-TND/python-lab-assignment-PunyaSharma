981de64fa82d228eb8b79bcf351015b6b84fe735
PunyaSharma <46519268+PunyaSharma@users.noreply.github.com>
2019-11-30 16:32:43 +0530

    Add files via upload

---
+++ s2.2.py
@@ -0,0 +1,12 @@
+c=(2,1,3,4,5,6)
+try:
+    c=5+'sj'
+    
+except AttributeError as e:
+    print("AttributeError",e)
+except TypeError as e:
+    print("TypeError",e)
+except ValueError as e:
+    print("ValueError",e)
+finally:
+    print("Process Complete")
